9ff61567bcdce3a7de0b6dcff70760aacb89b0b6
Alexander Blokhin <sashablokhindev@gmail.com>
2022-05-26 19:27:37 +0300

    Update MerryPhotoData.java

--- android/src/main/java/com/merryjs/PhotoViewer/MerryPhotoData.java
+++ android/src/main/java/com/merryjs/PhotoViewer/MerryPhotoData.java
@@ -1,11 +1,6 @@
 package com.merryjs.PhotoViewer;
 
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.views.imagehelper.ImageSource;
-
-/**
- * Created by bang on 26/07/2017.
- */
 
 public class MerryPhotoData {
     public ReadableMap source;
